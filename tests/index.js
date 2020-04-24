db58aa04931bfc17c06ac04473d7cf68543fb725
PabloFM <pafmon@gmail.com>
2020-04-24 11:33:08 +0200

    travis

--- tests/index.js
+++ tests/index.js
@@ -3,10 +3,11 @@ const newman = require('newman')
 newman.run({
     collection: require('./apiTests.json'),
     reporters: 'cli'
-}, function (err) {
-    if (err) {
+}, function (err, summary) {
+    var failuresCount = summary.run.failures.length;
+    if (failuresCount) {
         process.exit(1);
+    } else {
+        process.exit(0);
     }
-    console.log('collection run complete!');
-    process.exit(0);
 });
